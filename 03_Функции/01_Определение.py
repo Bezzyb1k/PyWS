158bed952fd6b09664528ef60c595b1917647929
Toothless <bezzybik.dmitry@yandex.ru>
2026-08-11 14:31:23 +0200

    update Определение: добавил пункт тесты

--- 03_Функции/01_Определение.py
+++ 03_Функции/01_Определение.py
@@ -8,15 +8,11 @@ def say_hello() -> None:
     """Выводит приветсвие в консоль"""
     print('Hello')
 
-say_hello()
-
 
 # Функция с одном параметром, принимает строку и ничего не возвращает
 def greet(name: str) -> None:
     """Выводит персонализированное приветствие"""
     print(f"Привет, {name}")
-
-greet('Toothless')
 
 
 # Функции с возратом значения, принимает два числа, возвращает число
@@ -24,15 +20,11 @@ def add(a: int, b: int) -> int:
     """Возвращает сумму двух чисел"""
     return a + b
 
-print(add(5, 10))
-
 
 # Функция с возвратом значения другого типа, принимает строку, возвращает число
 def get_length(text: str) -> int:
     """Возвращает длину строки"""
     return len(text)
-
-print(get_length('Python'))
 
 
 # Функция, которая может вернуть None 
@@ -41,9 +33,6 @@ def divide(a: int, b: int) -> float | None:
     if b == 0:
         return None
     return a / b
-
-print(divide(5, 5))
-print(divide(5, 0))
 
 
 # Документация функции (docstring) с указанием типов
@@ -60,5 +49,11 @@ def power(base: int, exp: int) -> int:
     """
     return base ** exp
 
-print(power(2, 3))
 
+# Тесты
+say_hello()
+greet('Toothless')
+
+print(f"5 + 10 = {add(5, 10)}")
+print(f"10 / 5 = {divide(10, 5)}")
+print(f"2 ** 3 = {power(2, 3)}")
